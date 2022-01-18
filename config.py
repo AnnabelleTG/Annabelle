43540d2e9680c35ca6faf7e7af0ab7ca70b73583
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-18 15:27:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from os import environ
+
+# vars that need to run the bot
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+HANDLER = environ.get("HANDLER")
