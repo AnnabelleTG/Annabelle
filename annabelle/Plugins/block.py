6901b243a1c1acb34a984eed86973489ddba7c9d
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-18 15:17:20 +0530

    Update block.py

--- annabelle/Plugins/block.py
+++ annabelle/Plugins/block.py
@@ -1,1 +1,29 @@
+#https://github.com/vaishnavofficial/Annabelle
 
+async def block(bot, message):
+       if message.from_user.id == MY_ID:
+         if message.reply_to_message:
+           ban_id = message.reply_to_message.from_user.id
+           await bot.block_user(ban_id)
+           await message.edit("`Haa! Blocked that user`")
+         else:
+           args = message.text.split(None, 1)
+           ban_name = args[1]
+           await bot.block_user(ban_name)
+           await message.edit("Blocked that user")
+
+async def unblock(bot, message):
+    try:
+      if message.from_user.id == MY_ID:
+         if message.reply_to_message:
+           ban_id = message.reply_to_message.from_user.id
+           await bot.unblock_user(ban_id)
+           await message.edit("`Haa! Unblocked that user`")
+         else:
+           args = message.text.split(None, 1)
+           ban_name = args[1]
+           await bot.unblock_user(ban_name)
+           await message.edit("Unblocked that user")
+    except Exception as e:
+         await message.edit("Something went wrong! Check app logs")
+         print(e)
