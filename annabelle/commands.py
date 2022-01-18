050adf488bb602cf561c8f536fdbffde327a04c4
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-18 15:05:30 +0530

    Create commands.py

---
+++ annabelle/commands.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters
+
+Vrn = Client(
+        api_id = API_ID,
+        api_hash = API_HASH
+        bot_token = BOT_TOKEN,
+        session_name = SESSION_STRING
+      )
