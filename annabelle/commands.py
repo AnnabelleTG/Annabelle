5709bbc7611e7ebfe66f95ea613c284968067a6a
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-18 15:25:07 +0530

    Update commands.py

--- annabelle/commands.py
+++ annabelle/commands.py
@@ -1,8 +1,14 @@
 from pyrogram import Client, filters
 
-Vrn = Client(
+from config import BOT_TOKEN, API_ID, API_HASH, HANDLER
+
+Annabelle = Client(
         api_id = API_ID,
         api_hash = API_HASH
         bot_token = BOT_TOKEN,
         session_name = SESSION_STRING
       )
+
+@Annabelle.on_message(filters.command('repo', HANDLER))
+async def repo(Client, message):
+     
