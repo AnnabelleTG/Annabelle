95926455cbba18bc05bd24c9358ce67a5ebdb931
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-28 18:12:58 +0530

    Update work.py

--- annabelle/funs/work.py
+++ annabelle/funs/work.py
@@ -1,1 +1,10 @@
+from annabelle.commands import Annabelle
+from pyrogram import filters as vrn
+from annabelle.helper_funcs.strings import WORK_TXT
 
+@bot.on_message(filters.command('work', '?'))
+async def work(bot, message):
+       if message.from_user.id == MY_ID:
+           await message.edit(WORK_TXT)
+       else:
+           return
