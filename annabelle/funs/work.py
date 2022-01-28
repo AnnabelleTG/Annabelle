23512acf7fe10989e969ac966079d8d4aba19333
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-28 20:53:48 +0530

    :-)

--- annabelle/funs/work.py
+++ annabelle/funs/work.py
@@ -3,7 +3,11 @@ from pyrogram import filters as vrn
 from annabelle.helper_funcs.strings import WORK_TXT
 
 async def work(Annabelle, message):
+     try:
        if message.from_user.id == MY_ID:
            await message.edit(WORK_TXT)
        else:
            return
+     except Exception as e:
+        message.edit(f'{e}')
+       
