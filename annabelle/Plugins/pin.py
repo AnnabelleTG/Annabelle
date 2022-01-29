84baeaccbad8836d0de1388f5f39c84e16519f4b
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-01-29 19:06:58 +0530

    pin haa

---
+++ annabelle/Plugins/pin.py
@@ -0,0 +1,26 @@
+from annabelle.commands import Annabelle
+from pyrogram import filters as vrn
+
+@Annabelle.on_message(filters.command('pin', '?') & filters.group)
+async def pin(Annabelle, message):
+    if not admins:
+        return
+    else:
+      try:
+        message_id = message.reply_to_message.message_id
+        await bot.pin_chat_message(message.chat.id, message_id)
+        await message.edit("<code>Pinned successfully!</code>")
+      except:
+        await message.edit("Reply to the message you want to pin")
+
+@Annabelle.on_message(vrn.command('unpin', '?'))
+async def unpin(Annabelle, message):
+    if not admins:
+        return
+    else:
+     try:
+        message_id = message.reply_to_message.message_id
+        await bot.unpin_chat_message(message.chat.id, message_id)
+        await message.edit("<code>Unpinned successfully!</code>")
+     except:
+        await message.edit("Reply to the message you want to unpin")
