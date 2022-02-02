aa1116cb31d8c5d88ffe755b58ecc8514abf62a2
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-02-02 16:49:50 +0530

    Fix

--- annabelle/__main__.py
+++ annabelle/__main__.py
@@ -6,15 +6,13 @@ from config import BOT_TOKEN, API_ID, API_HASH, HANDLER
 
 Annabelle = Client(
         api_id = API_ID,
-        api_hash = API_HASH
+        api_hash = API_HASH,
         bot_token = BOT_TOKEN,
         session_name = SESSION_STRING
-      )
+        )
 
 @Annabelle.on_message(vrn.command('repo', HANDLER))
 async def repo(Client, message):
-     message.edit("Hey, I am using Annabelle userbot"
-                  "Its easy and powerful. Deploy your own now"
-                  "https://github.com/Vaishnavofficial/Annabelle")
+    await message.edit("Hey, I am using Annabelle userbot\nIts easy and powerful. Deploy your own now\nhttps://github.com/Vaishnavofficial/Annabelle")
 
 Annabelle.run()
