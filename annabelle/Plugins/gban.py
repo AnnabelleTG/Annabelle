30504130b54a7fffd23f93790388ea4a2a834a71
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-04 16:22:57 +0530

    okda

--- annabelle/Plugins/gban.py
+++ annabelle/Plugins/gban.py
@@ -1,30 +1,28 @@
-from config import MY_ID
+from config import MY_ID, HANDLER
+from pyrogram import filters as vrn
 from annabelle.helper_funcs.admin_check import admin_check
 from annabelle.helper_funcs.strings import GBAN_TXT
 from annabelle.commands import Annabelle
+from annabelle.database.gban_db import add_gban, un_gban, check_gban
 
 import logging
+logging = logging.getLogger(__name__)
 
-G_BANS = dict({})
-
+@Annabelle.on_message(vrn.command('gban', HANDLER))
 async def gban(Annabelle, message):
-    if message.from_user.id == MY_ID
-      if message.reply_to_message:
-          if message.reply_to_message.from_user.id not in G_BANS.keys()
-             args = message.text.split(None, 1)
-             if len(args) >= 2:
-                 reason = args[1]
-             else:
-                 reason = None
-          G_BANS[message.reply_to_message.from_user.id] = reason if reason is not None else None
-          message.edit(GBAN_TXT.format(message.reply_to_message.from_user.mention, None if reason is None else reason))
-
-@Annabelle.on_message(filters.new_chat_members & filters.group)
-async def gban_kodk(Annabelle, message):
-     if message.from_user.id in G_BANS.keys():
-        member = await client.get_chat_member(chat_id=message.chat.id, user_id=MY_ID)
-        if member.can_delete_messages:
-           await Annabelle.kick_chat_member()
-           await message.edit(r"The recently joined user is globally banned. I have kicked him/her")
+    if message.from_user.id == MY_ID:
+        if message.reply_to_message:
+            args = message.text.split(None,1)
+            if len(args) >= 2:
+                reason = args[1]
+                id = message.reply_to_message.from_user.id
+                member = Annabelle.get_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                add_ban(id, reason)
+                await message.edit(GBAN_TXT.format(message.reply_to_message.from_user.mention, reason))
+            else:
+                reason = ""
         else:
-           await message.edit(r"Recently joined user is Globally banned! I can't kick him/her due to lack of permission")
+            await message.edit("You didn't specify whom to gban")
+   else:
+     return
+                
