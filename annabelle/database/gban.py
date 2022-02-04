c3658c0184b8c4947fb2bd7dcfb7ae2c23bc6bfb
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-04 15:39:04 +0530

    Update gban.py

--- annabelle/database/gban.py
+++ annabelle/database/gban.py
@@ -1,1 +1,34 @@
+import pymongo
+from pymongo import MongoClient
 
+from config import DATABASE_NAME, DATABASE_URL
+from annabelle.database.gmutes_db import  check_gmute, un_gmute
+
+client = MongoClient(DATABASE_URL)
+mydb = client[DATABASE_NAME]
+gban_col = mydb['GBANS']
+
+gmute_check = check_gmute()
+def add_gban(id, gmute_check, reason):
+  if gmute_check is True:
+    un_gmute()
+    x = gban_col.find_one(id)
+    if x:
+      return 
+    else:
+      return gban_col.insert_one({'_id' : id, 'reason' : reason})
+    
+def un_gban(id):
+  x = gban_col.find_one(id)
+  if x:
+    return gban_col.delete_one(id)
+  else:
+    return False
+  
+def chek_gban(id):
+  x = gban_col.find_one(id)
+  if x:
+    return True, reason if reason else None
+  else:
+    return False
+  
