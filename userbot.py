4503f6517e52ace94e422daa03de4699b0d85f42
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-10 15:32:21 +0530

    client

---
+++ userbot.py
@@ -0,0 +1,14 @@
+https://github.com/vaishnavoffic
+from pyrogram import Client
+from config import SESSION_STRING, API_iD , API_HASH
+
+Annabelle = Client( # userbot client  
+  api_id = API_ID,
+  api_hash = API_HASH,
+  bot_token = "xxxxxxx",
+  session_name = SESSION_STRING
+)
+
+
+print("Annabelle userbot has started!")
+Annabelle.run()
