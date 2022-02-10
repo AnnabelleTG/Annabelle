1d5f02ff26a4610d3461892486724498e7e0b6f5
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-10 15:24:15 +0530

    lol

---
+++ mainbot/botstart.py
@@ -0,0 +1,26 @@
+from pyrogram import filters as vrn
+from bot import mainbot
+from config import HANDLER, MY_ID
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import logging
+
+@mainbot.on_message(vrn.command('start', HANDLER) & vrn.private)
+async def start(mainbot, message):
+  await message.reply_photo(
+    photo = {}
+    caption = f"Heya {message.from_user}, I am a powerfull userbot for [this person](t.me/user?id={MY_ID}). The messages you send here will be forwarded to my master",
+    reply_markup = InlineKeyboardMarkup([
+      [InlineKeyboardMarkup("Deploy your own bot", url="https://github.com/vaishnavofficial/annabelle")],
+      [InlineKeyboardButton("Support Channel", url="t.me/XD_Botz"), InlineKeyboardButton("Group", url="t.me/vrn_group")]
+      ])
+  )
+    
+@mainbot.on_message(vrn.incoming & vrn.private)
+async def frwd(mainbot, message):
+  try:
+    USER_ID = message.from_user.mention
+    await mainbot.forward_message(message.from_user.id, MY_ID)
+  except:
+    mainbot.send_message(MY_ID, f"{USER_ID} is spamming me! I am not able to forward his messages")
+    
