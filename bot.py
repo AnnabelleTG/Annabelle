966d2babb888c329f3d84c3124d0c85a7fc1b682
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-10 15:37:38 +0530

    mainbot client

---
+++ bot.py
@@ -0,0 +1,12 @@
+from pyrogram import CLient
+from config import API_ID, API_HASH, BOT_TOKEN
+
+mainbot = Client(
+  api_id = API_ID,
+  api_hash = API_HASH,
+  bot_token = BOT_TOKEN,
+  session_name = "Annabelle-userbot")
+)
+
+print("MAinbot has started!")
+mainbot.run()
