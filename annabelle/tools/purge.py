52a5ce92294ab6aea243b4687171f68ff02dfa6d
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-11 16:07:45 +0530

    oru adaaru fix

--- annabelle/tools/purge.py
+++ annabelle/tools/purge.py
@@ -13,8 +13,8 @@ async def purge(bot:Client, msg:Message):
         admin = await bot.get_chat_member(chat_id, user_id=user_id)
         admin_type = ["adminsistrator", "creator"]
         count = 0
-        if admin.status == admin_type :
-            await bot.send_message(chat_id, text="**Starting to delete messages !!**")
+        if admin.status in admin_type :
+            await bot.send_message(chat_id, text="`Starting to delete messages !!`")
             for message in await bot.get_history(chat_id, offset_id=start, reverse=True) :
                 msg_id = message.message_id
                 await bot.delete_messages(chat_id, msg_id)
@@ -25,5 +25,5 @@ async def purge(bot:Client, msg:Message):
             await msg.edit_text(text="**Seems you are not an admin of the group**")
 
     except :
-        await msg.edit_text(text="**__Offset not specified !!__**")
+        await msg.edit_text(text="`Offset not specified !!`")
 
