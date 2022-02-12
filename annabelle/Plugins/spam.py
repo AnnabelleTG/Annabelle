18d91650713cd65d2458fef59a362088e1d63da8
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-12 20:04:50 +0530

    working ahno enh ariyilla #just_dance

---
+++ annabelle/Plugins/spam.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from annabelle.helper_funcs.PyroHelp import ReplyCheck
+from annabelle.helper_funcs.help import add_command_help
+
+
+@Client.on_message(filters.command("spam", ".") & filters.me)
+async def spam(bot, _, message: Message):
+    await message.delete()
+
+    times = message.command[1]
+    to_spam = " ".join(message.command[2:])
+
+    if message.chat.type in ["supergroup", "group"]:
+        for _ in range(int(times)):
+            await bot.send_message(
+                message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message)
+            )
+            await asyncio.sleep(0.20)
+
+    if message.chat.type == "private":
+        for _ in range(int(times)):
+            await bot.send_message(message.chat.id, to_spam)
+            await asyncio.sleep(0.20)
+
+
+# Command help section
+add_command_help("spam", [[".spam", "<spam_amount> <spam_text>"]])
