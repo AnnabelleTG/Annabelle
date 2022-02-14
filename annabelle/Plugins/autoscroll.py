a58598e4e6f2ab5ddf28304ef1a0ed6594636092
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-14 21:46:34 +0530

    may be this piece of code could work , small brain

---
+++ annabelle/Plugins/autoscroll.py
@@ -0,0 +1,34 @@
+"""
+ALL THIS CODE IS WRITTEN FOR ANNEBELLE USER BOT, ANY TYPE OF COPYRIGHT OR KANGING IS STRICTLY PROHIBITED AND MAY LEAD TO COPYRIGHT STRIKE
+THIS REPO IS UNDER MIT LICENCE 2022 
+CODE BY : [SAMINSUMESH](PAULWALKER_TG)
+
+"""
+
+from pyrogram import Client as Annabelle
+from pyrogram import filters
+from pyrogram.types import Message
+
+
+the_regex = r"^r\/([^\s\/])+"
+
+f = filters.chat([])
+
+
+@Annabelle.on_message(f)
+async def auto_read(_, message: Message):
+    await Annabelle.read_history(message.chat.id)
+    message.continue_propagation()
+
+
+@Annabelle.on_message(filters.command("autoscroll", HANDLER) & filters.me)
+async def add_to_auto_read(_, message: Message):
+    if message.chat.id in f:
+        f.remove(message.chat.id)
+        await message.edit("Autoscroll deactivated")
+    else:
+        f.add(message.chat.id)
+        await message.edit("Autoscroll activated")
+
+
+# This features helps to scroll to the new messages
