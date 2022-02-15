e88f5e70156d418dcd0985a46b93cb40ffff178d
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-15 11:44:08 +0530

    ok

--- annabelle/tools/alive.py
+++ annabelle/tools/alive.py
@@ -8,6 +8,6 @@ DEFAULT_IMG = "https://telegra.ph/file/0cf9d9cea0eebd03f6c1e.jpg"
 @Annabelle.on_message(vrn.command('alive', HANDLER))
 async def alive(Annabelle, message):
      if message.from_user.id == MY_ID:
-         await message.reply_photo(photo={},
+         await message.reply_photo(photo=DEFAULT_IMG,
                                    caption=ALIVE_TXT.format(ALIVE_EMOJI)
               )
