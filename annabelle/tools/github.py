fe3971848451b1077d727674468608147d0e16ea
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-15 11:41:23 +0530

    ok

--- annabelle/tools/github.py
+++ annabelle/tools/github.py
@@ -9,18 +9,20 @@ from pyrogram.types.messages_and_media import Message
 @Annabelle.on_message(filters.command("github", HANDLER))
 async def github(bot:Annabelle, msg:Message) :
     if msg.from_user.id == MY_ID :
-        query = msg.text.replace("?github ", "")
-        await msg.edit("`searching github...`")
-        try :
-            get_url = f"https://api.github.com/search/users?q={query}"
-            request1 = requests.get(get_url)
-            json_data = request1.json()
-            raw_data = json_data["items"]
-            data_ = raw_data[0]
-            url = data_["url"]
-            request2 = requests.get(url)
-            data = request2.json()
-            await msg.edit_text(text=f"""<p> **__Stdout__**
+        args = msg.text.split(None, 1)
+        if len(args) >= 2:
+            query = args[1]
+            await msg.edit("`searching github...`")
+            try :
+              get_url = f"https://api.github.com/search/users?q={query}"
+              request1 = requests.get(get_url)
+              json_data = request1.json()
+              raw_data = json_data["items"]
+              data_ = raw_data[0]
+              url = data_["url"]
+              request2 = requests.get(url)
+              data = request2.json()
+              await msg.edit_text(text=f"""<p> **__Stdout__**
 **Query** : `{query}`
 
 **ID** : `{data["id"]}`
@@ -35,6 +37,8 @@ async def github(bot:Annabelle, msg:Message) :
 
 **ANABELLE USERBOT**
 """)
-        except :
-            await msg.edit_text(text=f"**Couldnt find results for** `{query}`")
+           except :
+              await msg.edit_text(text=f"**Couldnt find results for** `{query}`")
+        else:
+            await message.edit("`Give a username to search`")
 
