e98456af6475ce816f0c55c4d5b81ea9a83852b1
Vaishnav <86590743+Vaishnavofficial@users.noreply.github.com>
2022-02-17 10:17:56 +0530

    Hmm🤔

--- annabelle/tools/autobio.py
+++ annabelle/tools/autobio.py
@@ -10,8 +10,9 @@ from pyrogram.types.messages_and_media import Message
 
 @Annabelle.on_message(vrn.command("autobio", HANDLER))
 async def autobio(bot:Annabelle, msg:Message) :
-    value = msg.text.split(" ")[1].lower
-    if value == "true" :
+    value = msg.text.split(None, 1)
+    args = value[1].lower()
+    if args == "true" :
         await msg.edit_text(text="**__Plugin Enabled__** : `autobio`")
         while value is "true" :
             await asyncio.sleep(60)
@@ -19,7 +20,7 @@ async def autobio(bot:Annabelle, msg:Message) :
             minute = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
             await bot.update_profile(bio=f"H : {hour} , M : {minute}")
 
-    elif value == "false" :
+    elif args == "false" :
         await msg.edit_text(text="**__Plugin Disabled__** : `autobio`")
 
     else :
