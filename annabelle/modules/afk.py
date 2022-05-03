8d63386495ff3a03f20eadc60999690f05170a39
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-03 23:17:02 +0530

    [Beta] AFK module v0.0.1
    * [Beta] AFK has been added 
    * This is just a simple version of AFK there will be an update soon 
    * [Beta] v0.0.1

---
+++ annabelle/modules/afk.py
@@ -0,0 +1,45 @@
+"""
+
+
+# Copyright Here
+
+
+"""
+import asyncio
+import datetime
+from pyrogram import Client, filters
+from pyrogram.handlers import (
+	MessageHandler
+	)
+from config import HANDLER
+from Annabelle.modules.helpers.afk_helpers import afk_handler
+
+@Client.on_message(filters.command("afk", prefixes=f"{HANDLER}") & filters.me)
+async def afk(client: Client, message):
+	global start, end, handler, reason
+	start = datetime.datetime.now().replace(microsecond=0)
+	handler = await client.add_handler(
+		MessageHandler(afk_handler, (filters.private & ~filters.me))
+		)
+		if len(message.text.split()) >= 2:
+			reason = message.text.split("", maxsplit=1)[1]
+		else:
+			reason = None
+            await message.edit("I'm going afk")
+            
+            
+@Client.on_message(filters.command("unafk", prefixes=f"{HANDLER}") & filters.me)
+async def unafk(client: Client, message):
+	try:
+		global start, end
+            end = datetime.datetime.now().replace(microsecond=0)
+            afk_dur = end - start
+            await message.edit(f"I'm not AFK any more. I was AFK {afk_dur}")
+      except NameError:
+      	await message.edit("You aren't AFK Now")
+      	await asyncio.sleep(3)
+      	await message.delete()
+      	
+modules_help.append(
+    {"afk": [{"afk [reason]": "Go to afk"}, {"unafk": "Get out of AFK"}]}
+)    
