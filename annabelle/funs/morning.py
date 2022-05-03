745bf1c9cd4382db50781bf018e2dffd24885aaa
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-03 22:07:56 +0530

    Delete annabelle/funs directory

--- annabelle/funs/morning.py
+++
@@ -1,9 +0,0 @@
-from annabelle import Annabelle
-from config import MY_ID
-from pyrogram import filters as vrn
-from annabelle.helper_funcs.strings import MORNING_TXT
-
-@Annabelle.on_message(vrn.command('morning', HANDLER))
-async def morning(Annabelle, message):
-      if message.from_user.id == MY_ID:
-          message.edit(MORNING_TXT)
