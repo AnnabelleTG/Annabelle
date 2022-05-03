6ebc9afb98bb2930b538ce16d881ba62797866d3
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-03 23:35:24 +0530

    Create afk_handler.py

---
+++ annabelle/modules/helpers/afk_handler.py
@@ -0,0 +1,17 @@
+# functions
+
+import datetime
+from pyrogram import Client, filters
+
+async def afk_handler(client, message):
+    try:
+        global start, end
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_dur = end - start
+        user_is_bot = message.from_user.is_bot
+        if user_is_bot is False:
+            await message.reply_text(
+                f"<b>I afk {afk_dur}</b>\n" f"<b>Reason:</b> <i>{reason}</i>"
+            )
+    except NameError:
+        pass
