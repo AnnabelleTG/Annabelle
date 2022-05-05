c0b85fbd399f5a22801cb8a47edb5d5f5bb101fd
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-05 15:01:23 +0530

    Update annabelle.py

--- annabelle.py
+++ annabelle.py
@@ -2,6 +2,18 @@ from pyrogram import Client, __version__
 from config import *
 import logging
 
+art = """
+
+   _____                      ___.          .__  .__          
+  /  _  \   ____   ____ _____ \_ |__   ____ |  | |  |   ____  
+ /  /_\  \ /    \ /    \\__  \ | __ \_/ __ \|  | |  | _/ __ \ 
+/    |    \   |  \   |  \/ __ \| \_\ \  ___/|  |_|  |_\  ___/ 
+\____|__  /___|  /___|  (____  /___  /\___  >____/____/\___  >
+        \/     \/     \/     \/    \/     \/               \/ 
+
+
+"""
+
 class Annabelle(Client):
 
     def __init__(self):
@@ -14,6 +26,7 @@ class Annabelle(Client):
 
     async def start(self):
         await super().start()
+        print(art)
         logging.info(f"Anabelle with for Pyrogram v{__version__} (Layer {layer}) started")
         
     async def stop(self, *args):
