4d685eb27181106057ff00d6036f4e22de5bed2f
Don <96370825+SflixBots@users.noreply.github.com>
2022-05-08 18:01:30 +0530

    Update __init__.py

--- annabelle/__init__.py
+++ annabelle/__init__.py
@@ -1,2 +1,21 @@
+from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
 
-# nothing Just annabelle
+LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+LOGDIR = f"{__name__}/logs"
+
+if not path.isdir(LOGDIR):
+    mkdir(LOGDIR)
+
+LOGFILE = f"{LOGDIR}/{__name__}_{LOG_DATETIME}_log.txt"
+
+file_handler = FileHandler(filename=LOGFILE)
+stdout_handler = StreamHandler(stdout)
+
+basicConfig(
+    format="%(asctime)s - [Alita_Robot] - %(levelname)s - %(message)s",
+    level=INFO,
+    handlers=[file_handler, stdout_handler],
+)
+
+getLogger("pyrogram").setLevel(WARNING)
+logger = getLogger(__name__)
