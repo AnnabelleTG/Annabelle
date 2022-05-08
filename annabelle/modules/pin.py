7a041ce11fc6113a8153e6892a78802a606b41a7
Don <96370825+SflixBots@users.noreply.github.com>
2022-05-08 17:02:43 +0530

    Update pin.py

--- annabelle/modules/pin.py
+++ annabelle/modules/pin.py
@@ -1,16 +1,16 @@
+from config import HANDLER
+from pyrogram import filters
 from annabelle import Annabelle 
-from pyrogram import filters as vrn
 
-@Annabelle.on_message(filters.command('pin', '?') & filters.group)
+@Annabelle.on_message(filters.command('pin', HANDLER) & filters.group)
 async def pin(Annabelle, message):
     if not admins:
         return
-    else:
-      try:
+    try:
         message_id = message.reply_to_message.message_id
         await bot.pin_chat_message(message.chat.id, message_id)
         await message.edit("<code>Pinned successfully!</code>")
-      except:
+    except:
         await message.edit("Reply to the message you want to pin")
 
 @Annabelle.on_message(vrn.command('unpin', '?'))
