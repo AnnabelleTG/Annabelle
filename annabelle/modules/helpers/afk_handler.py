bdf5a81f6881d08d47afd2ea46c7097ac471dc32
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-05-08 23:05:54 +0530

    Fix

--- annabelle/modules/helpers/afk_handler.py
+++ annabelle/modules/helpers/afk_handler.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 
 async def afk_handler(client, message):
     try:
-        global start, end
+        global start, end, reason
         end = datetime.datetime.now().replace(microsecond=0)
         afk_dur = end - start
         user_is_bot = message.from_user.is_bot
