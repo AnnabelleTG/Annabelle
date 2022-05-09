3af03c263491afea5d465af0991b28182bd76bf0
Don <96370825+SflixBots@users.noreply.github.com>
2022-05-09 09:07:22 +0530

    Update purge.py

--- annabelle/modules/purge.py
+++ annabelle/modules/purge.py
@@ -1,30 +1,35 @@
 import asyncio
-from pyrogram import Client, filters
-from pyrogram.types import Message
+
 from config import HANDLER
 from annabelle.modules.helpmenu.help_menu import modules_help
 
+from pyrogram.types import Message
+from pyrogram import Client, filters
 
-@Client.on_message(filters.command("del", prefixes=f"{HANDLER}") & filters.me)
+@Client.on_message(filters.command("del", HANDLER) & filters.me)
 async def del_msg(client: Client, message: Message):
-    if message.reply_to_message:
-        message_id = message.reply_to_message.message_id
-        await message.delete()
-        await client.delete_messages(message.chat.id, message_id)
-        k = await message.reply("I've deleted the message for you")
-        await asyncio.sleep(3)
-        await k.delete()
+    if not message.reply_to_message:
+        await message.reply_text("Reply to message to delete")
+        return
+    reply_message = message.reply_to_message
+    await message.delete()
+    await reply_message.delete()
+    k = await message.reply("I've deleted the message for you")
+    await asyncio.sleep(3)
+    await k.delete()
 
-@Client.on_message(filters.command("purge", prefixes=f"{HANDLER}") & filters.me)
+@Client.on_message(filters.command("purge", HANDLER) & filters.me)
 async def purge(client: Client, message: Message):
     messages_to_purge = []
-    if message.reply_to_message:
-        async for msg in client.iter_history(
-            chat_id=message.chat.id,
-            offset_id=message.reply_to_message.message_id,
-            reverse=True,
+    if not message.reply_to_message:
+        await message.reply_text("Reply to message to purge")
+        return
+    async for msg in client.iter_history(
+        chat_id=message.chat.id,
+        offset_id=message.reply_to_message.message_id,
+        reverse=True,
         ):
-            messages_to_purge.append(msg.message_id)
+        messages_to_purge.append(msg.message_id)
     for msgs in [
         messages_to_purge[i : i + 1000] for i in range(0, len(messages_to_purge), 1000)
     ]:
